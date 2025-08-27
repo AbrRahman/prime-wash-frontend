7c6093f7bfc5113f78d96ae1b45618824facfb82
AbrRahman <abrahman.est@gamil.com>
2025-08-27 10:06:44 +0600

    add Redux store configuration and custom hooks for dispatch and selector; update App component to use Redux Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import appRouter from "./routes/AppRouter";
+import { store } from "./redux/features/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
     <>
-      <RouterProvider router={appRouter} />
+      <Provider store={store}>
+        <RouterProvider router={appRouter} />
+      </Provider>
     </>
   );
 }
